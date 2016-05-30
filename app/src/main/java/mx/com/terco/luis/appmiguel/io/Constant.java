96414e98db5515cf9de6b335ebf53c4ee6e0b991
Luis Arias <lugearma@gmail.com>
2016-05-30 15:30:25 -0500

    initial commit

---
+++ app/src/main/java/mx/com/terco/luis/appmiguel/io/Constant.java
@@ -0,0 +1,15 @@
+package mx.com.terco.luis.appmiguel.io;
+
+/**
+ * Created by luisarias on 26/05/16.
+ */
+public class Constant {
+
+    public static final String BASE_URL = "http://www.serverbpw.com/cm/2016-2/secciones.php?idSec=";
+    public static final String PORTADA_PARAM = "1";
+    public static final String NACIONAL_PARAM = "2";
+    public static final String INTERNACIONAL_PARAM = "3";
+    public static final String DEPORTES_PARAM = "4";
+    public static final String ESPECTACULOS_PARAM = "5";
+
+}
---
+++ app/src/main/java/mx/com/terco/luis/appmiguel/io/VolleySingleton.java
@@ -0,0 +1,63 @@
+package mx.com.terco.luis.appmiguel.io;
+
+/**
+ * Created by luisarias on 26/05/16.
+ */
+
+import android.content.Context;
+import android.text.TextUtils;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by Luis on 04/11/2015.
+ */
+public class VolleySingleton {
+    public static final String TAG = VolleySingleton.class
+            .getSimpleName();
+
+    private static VolleySingleton mInstance;
+
+    private RequestQueue mRequestQueue;
+    private static Context mCtx;
+
+    private VolleySingleton(Context context) {
+        mCtx = context;
+        mRequestQueue = getRequestQueue();
+
+    }
+
+    public static synchronized VolleySingleton getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new VolleySingleton(context);
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+}
